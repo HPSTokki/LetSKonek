efb01ba679123b0669ff0fc32ca4b53049c83059
HPSTokki <sorachisenpai@gmail.com>
2024-10-24 18:51:32 +0800

    SCHOLARHIP AND JOB OPP UI UPDATE

--- js-front/burger-menu2.js
+++ js-front/burger-menu2.js
@@ -1,11 +1,35 @@
+// Variables go here for easy calling for later JS Functions
+
 const hamMenu = document.querySelector(".ham");
-
 const offScreenMenu = document.querySelector(".off-screen-menu")
-
 const wraptoggle = document.querySelector(".wrap")
+const faqsEvent = document.querySelector(".faqs")
+const scholar = document.getElementById("Scholarship")
+const jobOpp = document.getElementById("Job-Opp")
+
+
+// Hamburger Menu Interactivity
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle('active')
     offScreenMenu.classList.toggle('active')
     wraptoggle.classList.toggle('active')
+})
+
+
+
+// Link Events: Basically for redirecting to other webpage needed
+
+faqsEvent.addEventListener("click", () => {
+    window.location = "https://www.google.com"
+})
+
+scholar.addEventListener("click", ()=>{
+    window.location = "haha.html"
+
+})
+
+jobOpp.addEventListener("click", ()=>{
+    window.location = "haha.html"
+
 })
